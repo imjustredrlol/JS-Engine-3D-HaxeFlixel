f069d42b04a01c8c9150567fda9a65c1782145bd
imjustredrlol <145478881+imjustredrlol@users.noreply.github.com>
2023-11-18 13:10:52 -0600

    okno

--- source/import.hx
+++ source/import.hx
@@ -1,3 +1,3 @@
-import Paths;
+#if(!macro) import Paths; #end
 import haxe.ds.Vector as HaxeVector; //apparently denpa uses vectors, which is required for camera panning i guess
 import flixel.group.FlxSpriteGroup;
